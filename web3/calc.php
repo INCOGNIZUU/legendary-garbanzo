eeec65710281a9fe71e99d8730b7f51452f7ad60
INCOGNIZUU <fajiculayraymondc@gmail.com>
2020-11-08 06:07:07 -0800

    almost done 5-01 just need to figure out how to return hihi wala ako net eh

--- web3/calc.php
+++ web3/calc.php
@@ -3,14 +3,41 @@
 <body>
     <?php
         # test if input is_numeric
-        if(1){
+
+        $input1 = $_POST["val1"];
+        $input2 = $_POST["val2"];
+        $opt = $_POST["calc"];
+
+        //echo $input1;
+        //echo $input2;
+        //echo $opt;
+
+
+        if(is_numeric($input1) && is_numeric($input2)){
             # test if calc is not null
-            if(1){
+            if($opt != ""){
                 # switch statement for value of calc
-                switch(1){
-                    
+                //echo $opt;
+                switch($opt){
+                    case 'add':
+                        $add = $input1 + $input2;
+                        echo $add;
+                    break;
+                    case 'sub':
+                        $sub = $input1 - $input2;
+                        echo $sub;
+                    break;
+                    case 'mul':
+                        $mul = $input1 * $input2;
+                        echo $mul;
+                    break;
+                    case 'div':
+                        $div = $input1 / $input2;
+                        echo $div;
+                    break;
+
                 }
-                echo "Calculation result: $result";
+                //echo "Calculation result: $result";
             }
         }
         else{ echo "Invalid entry - please retry"; }
