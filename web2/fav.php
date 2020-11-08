861b66657f3961bdbd84d350cc393238bab4aef1
INCOGNIZUU <fajiculayraymondc@gmail.com>
2020-11-07 21:59:58 -0800

    Finished practice 5-00

--- web2/fav.php
+++ web2/fav.php
@@ -1,8 +1,26 @@
 <html>
-<head><title>Your Favorite</title></head>
-<body>
-    <?php
-        
-    ?>
-</body>
+    <head>
+        <title>Your Favorite</title>
+    </head>
+    <body>
+        <?php
+
+        $username = $_POST["username"];
+        $color = $_POST["color"];
+        $dish = $_POST["dish"];
+
+        if($username != null){
+            echo "Hello " . $username;
+            if($color != null && $dish != null){
+                echo " You really enjoy " . $dish . " especially with a nice " . $color . " wine";
+            }
+            else{
+                echo " You forgot to enter a dish or color of wine please go back to the previous page.";
+            }
+        }
+        else{
+            echo " Please enter your name on the previous page.";
+        }
+        ?>
+    </body>
 </html>
