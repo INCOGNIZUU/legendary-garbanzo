af2454179f338462a0334e682bb3dd2980740d54
Reven1126 <revenjeremiah@gmail.com>
2021-01-23 13:55:21 +0800

    web5

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,31 @@
+
 <?php
+
+session_start();
+///$_SESSION['count'] = 0;
+$_SESSION['count'] += 1;
+$num_visits = $_SESSION['count'];
+
+
+?>
+<body>
+<h1>Hello</h1>
+<p>You have visited this page <?php echo$num_visits;?> times</p>
+
+<?php
+
+if($num_visits >4){
+echo  "Thank you for visiting the page a lot.";
+}
+if ($num_visits == 10){
+
+    header('location: congratulations.php');
+}
+
+
 
 ?>
 
-<h1>Hello</h1>
-<p>You have visited this page <?= $num_visits ?> times</p>
-<p><a href="?reset=true">Reset</a></p>
+<p><a href="?link=1">Reset</a></p>
+
+</body>
